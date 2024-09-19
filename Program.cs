41c4194cc57d7e2c7f9314d0ab935a04a62d939f
UNIVERSE\eklimenko <eugene.klimenko@shift4.com>
2024-09-19 12:53:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static Transaction X(Transaction t)
+        {
+            switch (t.Type)
+            {
+                case 0:
+                    {
+                        t.DoSomething1();
+                        return t;
+                    }
+                case 1:
+                    {
+                        t.DoSomething2();
+                        return t;
+                    }
+                case 2:
+                    {
+                        t.DoSomething3();
+                        return t;
+                    }
+                case 3:
+                    {
+                        t.DoSomething4();
+                        return t;
+                    }
+                case 4:
+                    {
+                        t.DoSomething5();
+                        return t;
+                    }
+                default:
+                    {
+                        return null;
+                    }
+            }
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interview
+{
+    internal class Transaction
+    {
+        public int Type { get; }
+
+        public void DoSomething1() { }
+
+        public void DoSomething2() { }
+
+        public void DoSomething3() { }
+
+        public void DoSomething4() { }
+
+        public void DoSomething5() { }
+    }
+
+
+}
